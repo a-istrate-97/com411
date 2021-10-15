206a35b75c4491e41ce803bd36ee9d2476dfd677
Andrei Istrate <5istra55@solent.ac.uk>
2021-10-15 15:39:48 +0100

    Added code to demonstrate the use of multiple function calls.

---
+++ basics/functions/function_calls.py
@@ -0,0 +1,32 @@
+print("Please enter a word")
+word = input()
+
+def function(display_method):
+    if display_method == "box":
+        print("-----------")
+        print(f"[{word}")
+        print("-----------")
+    elif display_method == "lowercase":
+        print(word.lower())
+    elif display_method == "uppercase":
+        print(word.upper())
+    elif display_method == "mirrored":
+        reversed_word = word [::-1]
+        print(reversed_word)
+    elif display_method == "repeat":
+        print("How many times do you want the word to be repeated?")
+        repeat = int(input())
+        for i in range(repeat):
+            print(word)
+    else:
+        print("Invalid display method")
+
+def run():
+    function("box")
+    function("lowercase")
+    function("uppercase")
+    function("mirrored")
+    function("repeat")
+
+run()
+
