6c6db17c070fe04a27166620b683ddd1faf6de0d
Andrei Istrate <5istra55@solent.ac.uk>
2021-10-29 14:01:50 +0100

    Added code to demonstrate a simple list.

---
+++ data/lists/simple_list.py
@@ -0,0 +1,10 @@
+def directions():
+    directions = []
+    directions = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+    print(directions)
+
+def run():
+    directions()
+
+if __name__ == "__main__":
+    run()
