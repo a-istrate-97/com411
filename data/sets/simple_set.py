c842fcd0102d838c56b27928f38dfec1d897f98b
Andrei <5istra55@solent.ac.uk>
2021-11-05 21:59:19 +0000

    Added code to demonstrate a simple set.

---
+++ data/sets/simple_set.py
@@ -0,0 +1,11 @@
+def observed():
+    observations = set()
+    observations = {"Flying Car", "Sky Scraper", "Sky Scraper", "Laser", "Dome", "Dome"}
+    return observations
+
+def run():
+    observations = observed()
+    print(observations)
+
+if __name__ == "__main__":
+    run()
